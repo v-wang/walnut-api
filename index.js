66831753ec4ff8347428b14ebddabc48051ac845
v-wang <victorwang920@gmail.com>
2022-02-18 16:29:18 -0500

    created skeleton views

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-const port = 4000;
+const PORT = 4000;
 
-app.listen(port, () => [console.log(`Listening on port ${port}`)]);
+app.listen(PORT, () => [console.log(`Listening on port ${PORT}`)]);
