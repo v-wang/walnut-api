09e1fbcc531a1bfe78359bd0f8300dc834d46f87
v-wang <victorwang920@gmail.com>
2022-02-18 18:19:33 -0500

    modified URI connection to explicitly state address instead of localhost

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-
 dbName = 'walnut-api';
 
 const mongoURI =
   process.env.NODE_ENV === 'production'
     ? process.env.DB_URL
-    : `mongodb://localhost/${dbName}`;
+    : `mongodb://127.0.0.1/${dbName}`;
 
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((instance) => {
     console.log(`Connected to db: ${instance.connections[0].name}`);
   })
